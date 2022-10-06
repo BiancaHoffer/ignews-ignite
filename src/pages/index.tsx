e9042f2e01a8229ffd39a8aa694f5530981b754a
Bianca Macedo Hoffer Madruga <bianca.mhm1@gmail.com>
2022-10-06 16:23:10 -0300

    Atualização home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,7 @@ export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
-        <title>ig.newss</title>
+        <title>ig.news</title>
       </Head>
 
       <main className={styles.contentContainer}>
