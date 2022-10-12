747ac4d4a7136860256f175cf6167ae5482ed1b5
Bianca Macedo Hoffer Madruga <bianca.mhm1@gmail.com>
2022-10-12 19:13:10 -0300

    adicionando links no header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,22 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { SignInButton } from '../SignInButton';
 import styles from './styles.module.scss';
 
 export function Header() {
+  const { pathname } = useRouter();
+
   return (
     <header className={styles.headerContainer}>
         <div className={styles.headerContent}>
             <img src="/images/logo.svg" alt="ig.news" />
             <nav>
+              <Link href='/'>
                 <a className={styles.active}>Home</a>
+              </Link> 
+              <Link href='/posts'>
                 <a>Posts</a>
+              </Link>  
             </nav>
 
             <SignInButton />
